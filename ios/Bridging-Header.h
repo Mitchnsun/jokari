69d622e2964e5d2d8003abc37bb40058c08c82ec
matthieu_comperat <matthieu.comperat@gmail.com>
2015-05-15 19:15:33 +0200

    [v0.1] Add Facebook SDK - Login Button

---
+++ ios/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Jokari
+//
+//  Created by Matthieu Comperat on 15/05/2015.
+//  Copyright (c) 2015 Matthieu Comperat. All rights reserved.
+//
+
+#ifndef Jokari_Bridging_Header_h
+#define Jokari_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
